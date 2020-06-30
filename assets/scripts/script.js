580f642c3c3df266809d01e1f8dae8abbc1d5458
Nicole Graiff <nicole.graiff@gmail.com>
2020-06-30 17:02:48 -0400

    Added modal for project detals on portfolio page.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,36 +1,89 @@
 //Portfolio Page generation on menu click
 $('#portfolio').click(function(){
+    //clear all containers
     $('#mainContainer').empty();
     $('#home').attr('class', 'item');
     $('#portfolio').attr('class', 'active item');
     $('#contact').attr('class', 'item');
-    //portfolio image array
-    var myProjects = [{
-            name: 'Food App',
-            image: 'assets/images/annie-spratt-01Wa3tPoQQ8-unsplash.jpg',
-        },
-        {
-            name: 'Project Two',
-            image: 'assets/images/jasgleidy-duarte-3ZIgcJNSoQg-unsplash.jpg',
-        },
-        {
-            name: 'Project Three',
-            image: 'assets/images/laura-goodsell-DBcWaU5_AtE-unsplash.jpg',
-        },
-        {
-            name: 'Project Four',
-            image: 'assets/images/leandro-soengas-LwjdoCFew0w-unsplash.jpg',
-        },
-        {
-            name: 'Project Five',
-            image: 'assets/images/mana5280-dQ_RwYKmbrU-unsplash.jpg',
-        },
-        {
-            name: 'Project Six',
-            image: 'assets/images/marcella-marcella-hn6CC9aosEk-unsplash.jpg',
-        },
-    ]
     //build portfolio page
+    buildPortfolioPage(myProjects, projectDetails);
+});
+
+//portfolio image array
+var myProjects = [
+    {
+        name: 'Food App',
+        image: 'assets/images/ehimetalor-akhere-unuabona-Zh0mYmMBZjQ-unsplash.jpg',
+    },
+    {
+        name: 'Project Two',
+        image: 'assets/images/sumat-singh-TUTo_ON2w8o-unsplash.jpg',
+    },
+    {
+        name: 'Project Three',
+        image: 'assets/images/freya-ingva-6P9JgFe3f9Q-unsplash.jpg',
+    },
+    {
+        name: 'Project Four',
+        image: 'assets/images/leandro-soengas-LwjdoCFew0w-unsplash.jpg',
+    },
+    {
+        name: 'Project Five',
+        image: 'assets/images/yin-wong-SMFLG46c5S4-unsplash.jpg',
+    },
+    {
+        name: 'Project Six',
+        image: 'assets/images/marcella-marcella-hn6CC9aosEk-unsplash.jpg',
+    },
+]
+
+//project descriptions array
+var projectDetails = [
+    {
+        name: 'Food App',
+        description: 'A recipe search app to put customized menu planning for your next party at the tip of your fingers.',
+        image: 'assets/images/ehimetalor-akhere-unuabona-Zh0mYmMBZjQ-unsplash.jpg',
+        url: 'https://mackenzieraeclark.github.io/Project1/'
+    },
+    {
+        name: 'Project Two',
+        description: 'Coming Soon',
+        image: '',
+        url: '#'
+    },
+    {
+        name: 'Project Three',
+        description: 'Coming Soon',
+        image: '',
+        url: '#'
+    },
+    {
+        name: 'Project Four',
+        description: 'Coming Soon',
+        image: '',
+        url: '#'
+    },
+    {
+        name: 'Project Five',
+        description: 'Coming Soon',
+        image: '',
+        url: '#'
+    },
+    {
+        name: 'Project Six',
+        description: 'Coming Soon',
+        image: '',
+        url: '#'
+    }
+]
+
+function buildPortfolioPage(){
+    //empty containers
+    $('#mainContainer').empty();
+    $('#home').attr('class', 'item');
+    $('#portfolio').attr('class', 'active item');
+    $('#contact').attr('class', 'item');
+    //build the page
     var portfolioContainer = $('<div>').attr('class', 'container').appendTo('#mainContainer');
     $('<h1>').attr('class', 'ui header').text('Portfolio').appendTo(portfolioContainer);
     var mainCardContainer = $('<div>').attr('class', 'ui three column stackable grid').appendTo(portfolioContainer);
@@ -40,10 +93,54 @@ $('#portfolio').click(function(){
     var imageContainer = $('<div>').attr('class', 'centered column').appendTo(card);
     $('<img>').attr('src', myProjects[i].image).css('width', '100%').appendTo(imageContainer);
     var titleContainer = $('<div>').attr('class', 'content').appendTo(card);
-    $('<a>').attr('class', 'header').text(myProjects[i].name).appendTo(titleContainer);
+    var projectTitle = $('<a>').attr({class: 'header portfolioPage', id: i}).text(myProjects[i].name).appendTo(titleContainer);
+    //Create a modal with more information for clicked item
+    projectTitle.click(function(){
+        var selectedID = $(this).attr('id');
+        console.log('Selected id = ', $(this).attr('id'));
+        var showMeProject = projectDetails[selectedID];
+        console.log('The project title has been clicked. The associated id =', $(this).attr('id'));
+        //Initiate modal construction function
+        generateProjectModal(showMeProject)
+        });
     };
-});
+}
+//function to generate project detail modal
+function generateProjectModal(project){
+    //empty previous modal content
+    $('.ui.modal').empty();
+    console.log('generateProjectModal function has been entered.')
+    //set variables
+    var appName = project.name;
+        console.log('appName = ', appName);
+    var pic = project.image;
+        console.log('pic = ', pic);
+    var summary = project.description;
+        console.log('summary = ', summary);
+    var link = project.url;
+        console.log('link = ', link);
+    //build the modal
+    var modalContainer = $('<div>').attr('class','ui modal');
+    $('#mainContainer').append(modalContainer);
+    $('i').attr('class', 'close icon').appendTo(modalContainer);
+    $('<div>').attr('class', 'header').text(appName).appendTo(modalContainer);
+    var imageContainer = $('<div>').attr('class','image content').appendTo(modalContainer);
+    var imageDiv = $('<div>').attr('class', 'ui medium image').appendTo(imageContainer);
+    $('<img>').attr('src', pic).appendTo(imageDiv);
+    var descriptionContainer = $('<div>').attr('class', 'description').appendTo(imageContainer);
+    $('<div>').attr('class', 'ui header').text('Project Summary').appendTo(descriptionContainer);
+    $('<p>').text(summary).appendTo(descriptionContainer);
+    var buttonContainer = $('<div>').attr('class', 'actions').appendTo(modalContainer);
+    $('<div>').attr('class', 'ui black deny button').text('Back').appendTo(buttonContainer);
+    var projectLink = $('<div>').attr({class: 'ui positive button', src: link, target: '_blank'}).text('Visit Site').appendTo(buttonContainer);
+    projectLink.click(function(){
+        event.preventDefault();
+        window.location = link;
+    })
+    $('.ui.modal').modal('show');
+};
 
+// ********************* CONTACT PAGE ********************* 
 //Contact Page Generation on menu click
 $('#contact').click(function(){
     $('#mainContainer').empty();
@@ -64,5 +161,5 @@ $('#contact').click(function(){
     $('<label>').text('Message').appendTo(textContainer);
     $('<textarea>').appendTo(textContainer);
     var buttonContainer = $('<div>').appendTo(formContainer);
-    $('<div>').attr('class', 'ui submit button').text('Post Your Note').appendTo(buttonContainer);
+    $('<div>').attr('class', 'ui positive button').text('Post Your Note').appendTo(buttonContainer);
 });
